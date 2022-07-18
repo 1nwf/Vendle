9853041a5fe8911215bf26f5155b5a3ea37be578
Nawaf <nwf.aloufi@gmail.com>
2022-07-17 23:09:43 -0700

    removed unnecessary files

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,48 +1,12 @@
 import { Component } from "solid-js";
-import { Link, useRoutes, useLocation } from "solid-app-router";
-
-import { routes } from "./routes";
 
 const App: Component = () => {
-  const location = useLocation();
-  const Route = useRoutes(routes);
-
   return (
-    <>
-      <nav class="bg-gray-200 text-gray-900 px-4">
-        <ul class="flex items-center">
-          <li class="py-2 px-4">
-            <Link href="/" class="no-underline hover:underline">
-              Home
-            </Link>
-          </li>
-          <li class="py-2 px-4">
-            <Link href="/about" class="no-underline hover:underline">
-              About
-            </Link>
-          </li>
-          <li class="py-2 px-4">
-            <Link href="/error" class="no-underline hover:underline">
-              Error
-            </Link>
-          </li>
-
-          <li class="text-sm flex items-center space-x-1 ml-auto">
-            <span>URL:</span>
-            <input
-              class="w-75px p-1 bg-white text-sm rounded-lg"
-              type="text"
-              readOnly
-              value={location.pathname}
-            />
-          </li>
-        </ul>
-      </nav>
-
-      <main>
-        <Route />
-      </main>
-    </>
+    <div>
+      <h1 class="text-red-500 bg-black  p-4 rounded-xl m-12 hover:(text-white bg-red-500)">
+        Vendle
+      </h1>
+    </div>
   );
 };
 
