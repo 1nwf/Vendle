e7b44dcd93ab38c69f9716231bcf046a84eb8840
Nawaf <nwf.aloufi@gmail.com>
2022-07-21 00:47:00 -0700

    fix invalid paths

--- packages/main/constants.ts
+++ packages/main/constants.ts
@@ -1,7 +1,7 @@
 import { app } from "electron";
 
-export const NOTES_PATH = app.getPath("appData") + "/Vendle/extensions/";
-export const PLUGINS_PATH = app.getPath("appData") + "/Vendle/notes/";
-export const SETTINGS_PATH = app.getPath("appData") + "Vendle/settings.json";
+export const NOTES_PATH = app.getPath("appData") + "/Vendle/notes/";
+export const PLUGINS_PATH = app.getPath("appData") + "/Vendle/extensions/";
+export const SETTINGS_PATH = app.getPath("appData") + "/Vendle/user.json";
 export const WORKSPACE_FILES =
-  app.getPath("appData") + "Vendle/workspace_files.json";
+  app.getPath("appData") + "/Vendle/workspace_files.json";
