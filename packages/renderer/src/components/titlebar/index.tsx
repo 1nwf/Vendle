b8c5a95a49b465e3efbac9718c99777f0fbd4b43
Nawaf <nwf.aloufi@gmail.com>
2022-07-31 20:47:49 -0700

    increased titlebar z-index

--- packages/renderer/src/components/titlebar/index.tsx
+++ packages/renderer/src/components/titlebar/index.tsx
@@ -6,7 +6,7 @@ export default function Titlebar() {
   };
   return (
     <div
-      class={`w-screen  bg-transparent absolute pb-2 z-0 `}
+      class={`w-screen  bg-transparent absolute pb-2 z-50 `}
       style={`-webkit-app-region: drag;${settings.theme.appFg}`}
     >
       <div class="flex content-center items-center align-middle mt-1 ml-3">
