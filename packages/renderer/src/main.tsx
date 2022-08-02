1dddbc3a17948559f30d7db3b232d0563754d803
Nawaf <nwf.aloufi@gmail.com>
2022-08-02 19:59:43 -0400

    set up solid router

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -2,8 +2,36 @@
 import "virtual:windi.css";
 
 import { render } from "solid-js/web";
+import { Router, createIntegration } from "@solidjs/router";
 import App from "./app";
+function bindEvent(target: EventTarget, type: string, handler: EventListener) {
+  target.addEventListener(type, handler);
+  return () => target.removeEventListener(type, handler);
+}
 
+function electronIntegration() {
+  return createIntegration(
+    () => window.location.hash.slice(1),
+    ({ value, scroll }) => {
+      if (value.includes("index.html#")) {
+        value = new URL("file://" + value).hash;
+      }
+      window.location.hash = value.startsWith("/#/") ? value.slice(2) : value;
+      if (scroll) {
+        window.scrollTo(0, 0);
+      }
+    },
+    (notify) => bindEvent(window, "hashchange", () => notify()),
+    {
+      go: (delta) => window.history.go(delta),
+      renderPath: (path) => `#${path}`,
+    }
+  );
+}
 render(() => {
-  return <App />;
+  return (
+    <Router source={electronIntegration()}>
+      <App />
+    </Router>
+  );
 }, document.getElementById("root") as HTMLElement);
