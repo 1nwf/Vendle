a20bd01f60a24c2ae4b77b3c11d261ff74fd9983
Nawaf <nwf.aloufi@gmail.com>
2022-08-05 22:23:23 -0400

    show setting icon on sidepanel and open settings page in a modal

--- packages/renderer/src/components/sidepanel/index.tsx
+++ packages/renderer/src/components/sidepanel/index.tsx
@@ -1,26 +1,17 @@
-import { createSignal, Show } from "solid-js";
 import { settings } from "@/state/settings";
 import Workspace from "./Workspace";
 import AllFiles from "./files";
 import Plugins from "./plugins";
 import { Icon } from "solid-heroicons";
+import { createDisclosure, Modal, ModalOverlay } from "@hope-ui/solid";
+import { cog, collection, cube } from "solid-heroicons/outline";
+import { Settings } from "@/pages";
+import { createSignal, Show } from "solid-js";
 
-import { cube } from "solid-heroicons/outline";
-import { useNavigate } from "@solidjs/router";
 export default function SidePanel() {
   const [showExtensionsPage, setShowExtensionsPage] = createSignal(false);
-  const [showAllFiles, setShowAllFiles] = createSignal(true);
-  const navigate = useNavigate();
 
-  const handleShowExtensions = () => {
-    setShowAllFiles(!showAllFiles());
-    setShowExtensionsPage(!showExtensionsPage());
-    if (showExtensionsPage()) {
-      navigate("/plugins");
-    } else {
-      navigate("/");
-    }
-  };
+  const { isOpen, onOpen, onClose } = createDisclosure();
   return (
     <div class="mt-2">
       <Workspace />
@@ -31,7 +22,8 @@ export default function SidePanel() {
           }`}
         />
       </div>
-      <Show when={showAllFiles()}>
+
+      <Show when={!showExtensionsPage()}>
         <div class="px-4 rounded-r-2xl h-12/12">
           <AllFiles />
         </div>
@@ -42,15 +34,35 @@ export default function SidePanel() {
         </div>
       </Show>
 
-      <div class="absolute bottom-2 left-3">
-        <div class="flex">
-          <div onClick={handleShowExtensions}>
-            <Icon
-              path={cube}
-              class="h-6 w-6 mr-auto hover:cursor-pointer"
-              stroke-width={2}
+      <div class="absolute bottom-3 left-3">
+        <div class="grid grid-cols-3 gap-3">
+          <Icon
+            path={collection}
+            class="h-6 w-6 mr-auto hover:cursor-pointer"
+            stroke-width={2}
+            onClick={() => setShowExtensionsPage(false)}
+          />
+          <Icon
+            path={cog}
+            class="h-6 w-6 mr-auto hover:cursor-pointer"
+            stroke-width={2}
+            onClick={onOpen}
+          />
+          <Icon
+            path={cube}
+            class="h-6 w-6 mr-auto hover:cursor-pointer"
+            stroke-width={2}
+            onClick={() => setShowExtensionsPage(true)}
+          />
+          <Modal centered opened={isOpen()} onClose={onClose} size="7xl">
+            <ModalOverlay
+              bg="$blackAlpha3"
+              css={{
+                backdropFilter: "blur(10px) hue-rotate(90deg)",
+              }}
             />
-          </div>
+            <Settings onClose={onClose} />
+          </Modal>
         </div>
       </div>
     </div>
--- packages/renderer/src/pages/settings.tsx
+++ packages/renderer/src/pages/settings.tsx
@@ -1,3 +1,12 @@
-export default function Settings() {
-  return <div></div>;
+import { ModalBody, ModalContent, ModalHeader } from "@hope-ui/solid";
+
+export default function Settings({ onClose }: { onClose: () => void }) {
+  return (
+    <ModalContent>
+      <ModalHeader>
+        <h2>Settings</h2>
+      </ModalHeader>
+      <ModalBody class="my-4">settings..</ModalBody>
+    </ModalContent>
+  );
 }
