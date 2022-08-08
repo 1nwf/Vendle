29ca9ecc206f68472a6939832d1d5fa079e32393
Nawaf <nwf.aloufi@gmail.com>
2022-08-07 22:15:18 -0400

    prevent home page overlap with titlebar when sidebar is hidden

--- packages/renderer/src/pages/home.tsx
+++ packages/renderer/src/pages/home.tsx
@@ -3,10 +3,12 @@ import { settings } from "../state/settings";
 
 const App: Component = () => {
   return (
-    <div>
-      <h1 class="text-4xl" style={settings.theme.appFg}>
-        Choose a file
-      </h1>
+    <div class="mt-10 w-full justify-center">
+      <div class="ml-12">
+        <h1 class="text-4xl" style={settings.theme.appFg}>
+          Hello {settings.username}
+        </h1>
+      </div>
     </div>
   );
 };
