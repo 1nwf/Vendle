18ec1ea6b66ef52a603cce209fee7eb20b73c322
Nawaf <nwf.aloufi@gmail.com>
2022-08-26 19:12:16 -0400

    added types for settings object

---
+++ packages/types/settings.ts
@@ -0,0 +1,32 @@
+interface Colorscheme {
+  editorBg: string;
+  editorFg: string;
+  editorFont: string;
+  appBg: string;
+  appFg: string;
+  sidePanelBg: string;
+  sidePanelFg: string;
+  commandsPopupBg: string;
+  commandsPopupFg: string;
+}
+export interface Theme {
+  lightTheme: Colorscheme;
+  darkTheme: Colorscheme;
+}
+
+export interface Settings {
+  allowEditing: boolean;
+  menuItmes: never[];
+  isLightTheme: boolean;
+  lightTheme: Colorscheme;
+  darkTheme: Colorscheme;
+  overlayPopupCommands: {
+    tag: string;
+    name: string;
+  };
+  username: string;
+  themeName: string;
+  sidepanelShown: boolean;
+  pfpPath: "";
+  theme: Colorscheme;
+}
